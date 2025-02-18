17242b9e9d7b37581e4f7c84ff33895f7a74be86
CM <constantin.mader@gmail.com>
2025-02-18 12:44:49 +0100

    Adds simple UI and new logic

--- js/javascript.js
+++ js/javascript.js
@@ -36,45 +36,76 @@ function compareFinalScores(humanScore, computerScore){
 } 
 
 function playRound(humanChoice, computerChoice){
-    if (humanChoice === "paper" && computerChoice === "Rock" || humanChoice === "rock" && computerChoice === "Scissors" || humanChoice === "scissors" && computerChoice === "Paper" ){
-        console.log("You win! " + humanChoice + " beats " + computerChoice + "!")
-        return 0; //Human wins = 0
-    } else if (humanChoice === computerChoice.toLowerCase()){
-        console.log("It's a tie!");
-        return -1; // Tie = -1
+    const humanScoreElement = document.querySelector(".humanScore");
+    const computerScoreElement = document.querySelector(".computerScore");
+    const roundWinner = document.querySelector(".roundWinner");
+    const gameWinner = document.querySelector(".gameWinner");
+
+    gameWinner.textContent = "";
+    let humanScore = humanScoreElement.textContent;
+    let computerScore = computerScoreElement.textContent;
+
+   
+
+    if (humanChoice === "Paper" && computerChoice === "Rock" || humanChoice === "Rock" && computerChoice === "Scissors" || humanChoice === "Scissors" && computerChoice === "Paper" ){
+        roundWinner.textContent = "You win! " + humanChoice + " beats " + computerChoice + "!";
+        humanScore++; 
+        humanScoreElement.textContent = humanScore;
+        // return 0; //Human wins = 0
+    } else if (humanChoice === computerChoice){
+        roundWinner.textContent = "It's a tie!";
+        // return -1; // Tie = -1
     } else{
-        console.log("You loose! " + computerChoice + " beats " + humanChoice +"!")
-         return 1;// Computer wins = 1
+        roundWinner.textContent = "You loose! " + computerChoice + " beats " + humanChoice +"!";
+        computerScore++;
+        computerScoreElement.textContent = computerScore;
+        // return 1;// Computer wins = 1
     } 
+
+    if (humanScore === 5 || computerScore === 5){
+        if ( humanScore > computerScore){
+            gameWinner.textContent = "Congratulations! You won the game! Final Score: " + humanScore + " : " + computerScore ;
+        } else gameWinner.textContent = "Sorry! You lost the game, better luck next time! Final Score: " + humanScore + " : " + computerScore ;
+        humanScoreElement.textContent = 0;
+        computerScoreElement.textContent = 0;
+    }
 } 
 
-function playGame(){
-    let humanScore = 0;
-    let computerScore = 0;
-    let roundsPlayed = 0;
+const buttonCointainer = document.querySelector(".container");
 
-    while (roundsPlayed < 5){ 
-        const humanChoice = getHumanChoice();
-        const computerChoice = getComputerChoice();
-        console.log("Playing Round " + parseInt(roundsPlayed+1));
-        roundWinner = playRound(humanChoice, computerChoice);
+buttonCointainer.addEventListener("click", (event) =>{
+    const computerChoice = getComputerChoice();
+    const humanChoice = event.target.outerText;
+    playRound(humanChoice,computerChoice);
+});
+
+// function playGame(){
+//     let humanScore = 0;
+//     let computerScore = 0;
+//     let roundsPlayed = 0;
+
+//     while (roundsPlayed < 5){ 
+//         const humanChoice = getHumanChoice();
+//         const computerChoice = getComputerChoice();
+//         console.log("Playing Round " + parseInt(roundsPlayed+1));
+//         roundWinner = playRound(humanChoice, computerChoice);
         
-        if (roundWinner == 0){
-            humanScore++;
-        } else if (roundWinner == 1){
-            computerScore++;
-        } 
-        console.log("Current Score: Human: " + humanScore + " Computer: " + computerScore);
-        roundsPlayed++;
-    } 
-    compareFinalScores(humanScore, computerScore);
-}  
+//         if (roundWinner == 0){
+//             humanScore++;
+//         } else if (roundWinner == 1){
+//             computerScore++;
+//         } 
+//         console.log("Current Score: Human: " + humanScore + " Computer: " + computerScore);
+//         roundsPlayed++;
+//     } 
+//     compareFinalScores(humanScore, computerScore);
+// }  
 
 function getRandomInt() {
     return Math.floor(Math.random() * 3);
   }
 
-playGame();
+// playGame();
 
 
   
